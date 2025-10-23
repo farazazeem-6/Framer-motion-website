998a9b0c3acfd229efeb6c64629edf9ad8e47a1e
farazazeem-6 <faraz.tacticminds.com>
2025-10-23 15:10:49 +0500

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../../components/Header/Header.module.css";
+
+function Header() {
+  return <div className={styles.header}>
+    
+  </div>;
+}
+
+export default Header;
