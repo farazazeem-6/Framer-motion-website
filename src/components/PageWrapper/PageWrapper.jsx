da0a5732869dfddc42b527c5d5f9a2676d93aacb
faraz-dhillon <faraz.tacticminds@gmail.com>
2025-11-05 03:40:53 +0500

    theme done

--- src/components/PageWrapper/PageWrapper.jsx
+++ src/components/PageWrapper/PageWrapper.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function PageWrapper({ children }) {
   const pageWrapper = {
     paddingTop: "160px",
-    backgroundColor: "rgb(21,28,44)",
+    backgroundColor: "var(--mobile-menu-bg)",
   };
   return <div style={pageWrapper}>{children}</div>;
 }
